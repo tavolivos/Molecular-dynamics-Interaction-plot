3656d53d700250c15b9ef1943a6e08d8cd7c094c
Gustavol Enrique Olivos Ramirez <tavolivos@gmail.com>
2021-01-07 02:23:22 -0500

    Add files via upload

---
+++ get_pdb_frames.tcl
@@ -0,0 +1,8 @@
+mol new md_0_10.gro
+animate delete  beg 0 end 0
+mol addfile {005_PS-002.xtc} type {xtc} first 0 last 9999 step 10
+#Para cargar un archivo pdb
+#mol addfile {out_003_PS_PDBtrajectories.pdb} type {pdb} first 1 last 20002 step 10
+set sel [atomselect top "protein or resname UNK"]
+animate write pdb {name.pdb} beg 0 end -1 sel $sel 
+#exit
